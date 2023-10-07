b709b93adfe2c7b69e51dbf322ea48c1b2a057ad
sktt1faker02 <you@example.com>
2023-10-07 17:02:06 +0800

    add linkedin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { BiLogoLinkedin, BiLogoGithub, BiLogoFacebook } from "react-icons/bi";
 const Footer = () => {
   return (
     <footer className="footer">
-      <a href="/">
+      <a href="https://www.linkedin.com/in/michael-tabilin-49811a20b/">
         <BiLogoLinkedin />
       </a>
       <a href="https://github.com/sktt1faker02">
