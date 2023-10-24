3f0301cf6940fd63c77460805b840c4f7fd25d93
sktt1faker02 <you@example.com>
2023-10-24 10:56:52 +0800

    Add cofferoaster subscription site project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
         </li>
         <li>
           <BiLogoSass />
-          SASS
+          SCSS
         </li>
         <li>
           <BiLogoTailwindCss />
--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import multistepformImg from "./assets/images/multi-step-form.png";
 import ecommerceImg from "./assets/images/e-commerce.png";
-import loopstudiosImg from "./assets/images/loop-studios.png";
+import cofferoasterImg from "./assets/images/coffee-roaster-site.png";
 import restcountriesImg from "./assets/images/rest-countries.png";
 
 export const links = [
@@ -25,30 +25,31 @@ export const links = [
 export const projects = [
   {
     id: 1,
+    img: cofferoasterImg,
+    title: "coffeeroaster subscription site",
+    description: "Discover a world of premium coffee with our subscription service. Enjoy hand-roasted beans from around the globe, delivered fresh to your door.",
+    stack: ["react", "react-router", "scss"],
+    live: "https://coffeeroasters-subscription-site.michaeltabilin.dev/",
+    repo: "https://github.com/sktt1faker02/coffeeroasters-subscription-site",
+  },
+
+  {
+    id: 2,
     img: multistepformImg,
     title: "multi step form",
     description: "In this component, users complete each step of the sequence. Users can go back to a previous step to update their selections and also view a summary of their selections on the final step before confirming their order.",
-    stack: ["html", "css", "javascript", "react", "sass"],
+    stack: ["html", "css", "javascript", "react", "scss"],
     live: "https://sktt1faker02-multi-step-form.vercel.app/",
     repo: "https://github.com/sktt1faker02/multi-step-form",
   },
   {
-    id: 2,
+    id: 3,
     img: ecommerceImg,
     title: "e-commerce product page",
     description: "A product page where users can view the product in a lightbox gallery, switch the large product image by clicking on the small thumbnail images, add items to the cart, view the cart, and remove items from it.",
     stack: ["html", "css", "javascript", "react", "tailwind"],
     live: "https://sktt1faker02-ecommerce-product-page.vercel.app/",
     repo: "https://github.com/sktt1faker02/Ecommerce-Product-Page",
-  },
-  {
-    id: 3,
-    img: loopstudiosImg,
-    title: "loopstudios landing page",
-    description: "This is a landing page for a virtual reality production company, designed to showcase their company, products, and services",
-    stack: ["html", "css", "javascript"],
-    live: "https://sktt1faker02-loopstudios-landing-page.netlify.app/",
-    repo: "https://github.com/sktt1faker02/Frontend-Mentor-Projects/tree/master/loopstudios-landing-page-main",
   },
   {
     id: 4,
